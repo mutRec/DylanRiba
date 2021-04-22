63a331cca3a5f539150ff23eb4177be78f0c1252
jordi carlos <jordi.carlos@rusc.net>
2021-04-23 00:35:05 +0200

    afegiment seccions web

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 // Importem mòduls Nodejs (express framework).
-express = require('express');
-const app = express;
+const express = require('express');
 
 // Creem el servidor. Aplicació que escolti al port 5001.
+const app = express();
+const port = 5001;
 
+app.listen(port, () => {
+    console.log(`Servidor escoltant port ${port}`)
+});
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+});
